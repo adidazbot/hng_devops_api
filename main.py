21a8959236130bba1b7b6a7c501a3eb964b9f40e
adidazbot <ayenicharlesadidaz@ggmail.com>
2025-02-07 23:49:43 +0100

    20th push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
-app = Flask(__name__)
+app = Flask(my_app)
 CORS(app)  # Enable CORS for all routes
 
 # Helper functions
@@ -59,5 +59,5 @@ def classify_number():
     }
     return jsonify(response), 200
 
-if __name__ == '__main__':
+if my_app == '__main__':
     app.run(debug=True)
